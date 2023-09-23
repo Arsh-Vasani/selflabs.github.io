7d8b88117f8da80163ae6b13b1e7c6069a639e8c
Arsh-Vasani <130985822+Arsh-Vasani@users.noreply.github.com>
2023-09-23 15:20:51 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,58 @@
+const nexticon = '<img src="../images/next-btn.png" alt="next">';
+const prewicon = '<img src="../images/prew-btn.png" alt="prew">';
+$('.goals').owlCarousel({
+    loop:false,
+    nav:true,
+    dots:false,
+    navText: [
+        prewicon,
+        nexticon 
+    ],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+$('.companies').owlCarousel({
+    loop:false,
+    nav:true,
+    dots:false,
+    navText: [
+        prewicon,
+        nexticon 
+    ],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+$('.last-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:3
+        }
+    }
+})
